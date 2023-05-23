be0b1e6c6aa667baf8dc62e4a0114f32313a53b3
XcarabuzX <xcarabuzx@gmail.com>
2023-05-22 20:15:28 -0400

    agregando funcion y estado para definir ganador

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i){
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -22,8 +22,17 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Han ganado las: " + winner;
+  }else{
+    status = "Siguiente jugador: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <div className='board'>
+      <div className="status">{status}</div>
       <div className='board-row'>
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -43,3 +52,22 @@ export default function Board() {
   );
 }
 
+function calculateWinner (squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
